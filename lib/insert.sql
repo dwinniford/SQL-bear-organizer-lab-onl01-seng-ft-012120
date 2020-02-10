3a6a92614faeb0b3d434089e29a4dce2acb3ba17
Dave Winniford <dwinniford@gmail.com>
2020-02-10 20:13:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 6, "male", "brown", "sweet", )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 6, "male", "brown", "sweet", 1)
 INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
