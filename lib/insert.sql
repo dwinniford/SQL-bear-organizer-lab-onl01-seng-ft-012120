7c0043206fa988d7b2362fc65e0fc199408e430a
Dave Winniford <dwinniford@gmail.com>
2020-02-10 20:21:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 26, "female", "black", "wild", 30);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 17, "male", "green", "warm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 13, "female", "brown", "sweet", 0);
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES (13, "female", "brown", "angry", 0);
